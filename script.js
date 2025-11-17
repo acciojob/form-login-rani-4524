09b6e4c4f8bc9ca2718da216ca324b643bcd76fc
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-17 16:54:00 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
+	// const submitBtn = document.querySelector("#submit");
+	// submitBtn.addEventListener ("click" , (e)=>{
+		e.preventDefault();
+		const firstName = document.forms["form1"]["fname"].value;
+		const lastName = document.forms["form1"]["lname"].value;
+
+		alert(firstName + " " + lastName);
+		
+	})
 
 }

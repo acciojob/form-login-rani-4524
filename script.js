daee65810b4e23324455ec88f68c117b47a12121
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-17 16:59:45 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function getFormvalue(e) {
+function getFormvalue() {
     //Write your code here
 	// const submitBtn = document.querySelector("#submit");
 	// submitBtn.addEventListener ("click" , (e)=>{
-		e.preventDefault();
-		const firstName = document.forms["form1"]["fname"].value;
-		const lastName = document.forms["form1"]["lname"].value;
+		// event.preventDefault();
+		const firstName = document.forms["form1"]["fname"].value.trim();
+		const lastName = document.forms["form1"]["lname"].value.trim();
 
+	    if (firstName === "" && lastName === "") {
+      alert("Please enter at least one name.");
+      return;
+    }
 		alert(firstName + " " + lastName);
-		
-	})
 
 }
